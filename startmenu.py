1e589b6f31c778180ce02502b1ed588251e8a71e
samdi0316 <92657345+samdi0316@users.noreply.github.com>
2023-01-22 13:11:53 +0000

    Changes to be committed: 	new file:   startmenu.py

---
+++ startmenu.py
@@ -0,0 +1,55 @@
+import curses
+from curses import wrapper
+from curses.textpad import Textbox, rectangle
+import os
+import os.path
+import time
+
+def main(stdscr):
+    ##설정준비:미리 자주사용할 함수나 색상들을 설정
+    ##설정준비:색상
+    curses.init_pair(1,curses.COLOR_WHITE,curses.COLOR_BLACK)     
+    curses.init_pair(2,curses.COLOR_BLACK,curses.COLOR_WHITE)  
+       
+    MAIN_COLOR=curses.color_pair(1)
+    MAIN_COLOR_REVERSE=curses.color_pair(2)
+
+    ##설정준비:터미널 사이즈 조사
+    R_row,R_coulumn=os.popen('stty size', 'r').read().split()
+    row=int(R_row)
+    coulmns=int(R_coulumn)
+
+    ##설정준비:파일 검색 함수
+    def findfile(name, path):
+        for dirpath, dirname, finding_name in os.walk(path):
+            if name in finding_name:
+                return os.path.join(dirpath, name)
+
+    ##첫화면:실행 모드 선택
+    ##첫화면:화면분할
+    RIGHT_WINDOW=curses.newwin(1,coulmns//2,row-2,coulmns//2-1)
+    LEFT_WINDOW=curses.newwin(1,1,coulmns//2-2)
+
+    ##첫화면:실행모드 선택 메뉴
+    LEFT_WINDOW.clear()
+    LEFT_WINDOW.addstr(row//2-2,1,"Ⅰ:OPERATE SYS")
+    LEFT_WINDOW.addstr(row//2,1,"Ⅱ:SYS SETTING")
+    LEFT_WINDOW.addstr(row//2+2,1,"Ⅲ:SYS OFF")
+    LEFT_WINDOW.refresh()
+
+    ##첫화면:버튼입력에 따라 실행모드 선택
+    while True:
+        BUTTON_input=stdscr.getch()
+        if BUTTON_input==1:
+            stdscr.clear()
+            
+
+    stdscr.clear()
+    stdscr.addstr(row-2,0,"Ⅰ    Ⅱ   Ⅲ   Ⅳ   Ⅳ   Ⅵ".center(coulmns),WB_color)
+    stdscr.addstr(row-3,0,"SELET MODE".center(coulmns))
+    stdscr.refresh()
+    
+    stdscr.getch()
+
+
+wrapper(main)
