06b64dc5c370a19d74f9ff8022ae695036b1e5d0
Guest <guest-qxuy18@h23754.sgb.skir.kr>
2023-02-16 23:27:51 +0900

    졸려서 뭐한거인지 모름

--- MAIN_MENU.py
+++ MAIN_MENU.py
@@ -29,12 +29,12 @@ def main(stdscr):
                 return os.path.join(dirpath, name)
 
     ##설정준비:화면분할 
-    TOP_WINDOW=curses.newwin(1,coulmns,0,0)
-    RIGHT_WINDOW=curses.newwin(row-3,coulmns//2-1,1,coulmns//2)
-    LEFT_WINDOW=curses.newwin(row-3,coulmns//2-2,1,1)
+    TOP_WINDOW=curses.newwin(2,coulmns,0,0)
+    RIGHT_WINDOW=curses.newwin(row-4,coulmns//2+16,2,15)
+    LEFT_WINDOW=curses.newwin(row-4,15,2,1)
     BOTTOM_WINDOW=curses.newwin(2,coulmns,row-2,0)
 
-##메뉴설정:세부 메뉴들을 함수 형태로 미리 설정
+##메뉴설정:세부 메뉴들을 딕셔너리 형태로 미리 설정
     ##통신메뉴:와이파이,I2C,시리얼 통신을 설정,송수신 표시
     ##통신메뉴:와이파이
     
@@ -49,6 +49,37 @@ def main(stdscr):
     ##설정메뉴
 
 ##초기화면:설정한 메뉴들중에서 선택, 실행
+    ##초기화면:현재 커서가 있는 위치를 저장
+    COSOR = 0
+    COS_DIR = "/MAIN_MENU"
+
+    RIGHT_WINDOW.clear()
+    RIGHT_WINDOW.addstr(row//2-2,coulmns//2-21,"use arrow key")
+    RIGHT_WINDOW.addstr(row//2-1,coulmns//2-21,"to select menu")
+    RIGHT_WINDOW.refresh()
+
+    ##초기화면:좌측 초기 매뉴 표시
+    LEFT_WINDOW.clear()
+    LEFT_WINDOW.addstr(1,0,"MAIN MENU".center(15),MAIN_COLOR_REVERSE)
+    LEFT_WINDOW.addstr(2,0,'commuication'.center(15))
+    LEFT_WINDOW.addstr(3,0,'GPIO'.center(15))
+    LEFT_WINDOW.addstr(4,0,'setting'.center(15))
+    LEFT_WINDOW.refresh()
+
+    ##첫화면:상단바 표시
+    TOP_WINDOW.clear()
+    TOP_WINDOW.addstr(0,0,"hi".ljust(coulmns-1),MAIN_COLOR_REVERSE)
+    TOP_WINDOW.addstr(0,coulmns-5,"DATE",MAIN_COLOR_REVERSE)
+    TOP_WINDOW.addstr(1,0,f"{COS_DIR}".ljust(coulmns-1),MAIN_COLOR_REVERSE)
+    TOP_WINDOW.refresh()
+
+    ##첫화면:하단바 표시
+    BOTTOM_WINDOW.clear()
+    BOTTOM_WINDOW.addstr(0,0,"SELET MENU".center(coulmns-1))
+    BOTTOM_WINDOW.addstr(1,0,"Ⅰ    Ⅱ   Ⅲ   Ⅳ   Ⅳ   Ⅵ".center(coulmns-1),MAIN_COLOR_REVERSE)
+    BOTTOM_WINDOW.refresh()
+
+    LEFT_WINDOW.getch()
 
 
 
