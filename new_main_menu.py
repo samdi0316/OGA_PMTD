17502aa354c4a1eeb1004ab49c9b8eaf767312b0
samdi0316 <92657345+samdi0316@users.noreply.github.com>
2023-08-10 13:44:50 +0000

    세부 메뉴 진입 화면 완성

--- new_main_menu.py
+++ new_main_menu.py
@@ -110,16 +110,24 @@ def menu_vertical_1(selected_window, menu_item, selected_row, middle_hight, midd
 			selected_window.addstr(item_y, item_x, item)
 	selected_window.refresh()
 
-##그리기함수:세로로 메뉴를 나열(딕셔너리 사용)
-def menu_vertical_dic(selected_window, menu_item, selected_row, middle_hight, middle_width):
-	for index, (key, item) in enumerate(Main_menu.items[menu_item]):
-		item_x = middle_width - len(item)//2
-		item_y = middle_hight - len(menu_item) + index * 2
-	
+##그리기함수:세로로 메뉴를 나열(딕셔너리 사용)(키값에따라 하단부에 설명 추가)
+def menu_vertical_dic(selected_window, dic_menu ,menu_item, selected_row, middle_hight, middle_width, max_hight, max_width, max_width_left):
+	for index, (key, item) in enumerate(dic_menu.items[menu_item]):
+		key_x = middle_width - len(key)//2
+		key_y = middle_hight - len(dic_item) + index * 2 - 7
+
 		if index == selected_row:
-			selected_window.addstr(item_y, item_x, item, SELECT_COLOR)
-		else:
+			selected_window.addstr(key_y, key_x, item, SELECT_COLOR)
+
+			item_x = middle_width - len(item)//2
+			item_y = max_hight - 9 
+			selected_window.addstr(max_width_left + 2, item_y - 2, )
+			for i in range(max_width_left + 2, max_width - 2):
+				selected_window.addstr(item_y -2 , i,"─")
 			selected_window.addstr(item_y, item_x, item)
+		else:
+			selected_window.addstr(key_y, key_x, item)
+
 	selected_window.refresh()
 
 ##그리기함수:가로로 메뉴를 나열
@@ -191,37 +199,65 @@ def main(stdscr):
 	stdscr.refresh()
 
 	##메인 메뉴 선택 작동
-	selected_main_menu = 99
 	while True:
 		key = stdscr.getch()
 		if key == curses.KEY_UP and selected_left_menu > 0:
 			selected_left_menu -= 1
 		elif key == curses.KEY_DOWN and selected_left_menu < len(Main_menu)-1 :
 			selected_left_menu += 1
-		elif key == curses.KEY_ENTER or key in [10, 13]:	
-			selected_left_menu = selected_main_menu
+		elif key == curses.KEY_ENTER or key in [10, 13]:
+			selected_main_menu = list(Main_menu.keys())[selected_left_menu]
 			break
 
 		menu_vertical_1(stdscr, Main_menu, selected_left_menu, left_screen_hight//2, left_screen_width//2 )
 		stdscr.refresh()
 
 	
-	##선택한 메인 메뉴에 맞추어서 세부 메뉴 표시
-	right_cursor = 0
+	##선택한 메인 메뉴에 맞추어서 화면 변경
 
-	while Ture:
+	##이전 화면 다 지우고 상단,하단화면 다시 표시
+	##맨 위 화면은 [사용자						날짜] 형식으로 표시
+	stdscr.clear()
+	stdscr.addstr(top_screen_hight, 0, DATE.rjust(top_screen_width," "), MAIN_COLOR_REVERSE)
+	stdscr.addstr(top_screen_hight, 0, USER,MAIN_COLOR_REVERSE)
+	
+	##맨 밑 화면 첫번째 줄은 D-PAD를 이용하라는 문구, 두번째 줄은 특수 버튼 지정(현재는 가장 우측것만 종료)
+	stdscr.addstr(bot_screen_hight, 0, "Use D-PAD and A button to select menu".center(bot_screen_width-1, ' '), MAIN_COLOR_REVERSE)
+	stdscr.addstr(bot_screen_hight+1, 0, "SHUT DOWN  ".rjust(bot_screen_width-1, ' '))
+	
+	##중앙 좌측화면과 우측화면을 나누는 선 그리기
+	for i in range(top_screen_hight+3, bot_screen_hight-3):
+		stdscr.addstr(i, left_screen_width,"│")
+	
+
+	##좌측 상단에 선택한 메뉴 하이라이트 표시
+	stdscr.addstr(top_screen_hight+4, (left_screen_width - len(selected_main_menu))//2, selected_main_menu, curses.A_BOLD )
+
+	##좌측 화면에 세부 메뉴 표시
+	selected_left_menu = 0
+	menu_vertical_1(stdscr, Main_menu[selected_main_menu], selected_left_menu, left_screen_hight//2 +1, left_screen_width//2 )
+	stdscr.refresh()
+
+	##우측 화면에 선택된 세부메뉴 설명 표시
+	stdscr.addstr((right_screen_hight -top_screen_hight)//2 +top_screen_hight, (right_screen_width -left_screen_width)//2 +left_screen_width -len(list(Main_menu[selected_main_menu].values())[0])//2, list(Main_menu[selected_main_menu].values())[0])
+	stdscr.refresh()
+
+	##좌측 메뉴 선택 작동 및 우측 설명칸 작동
+	while True:
 		key = stdscr.getch()
-		if key == curses.KEY_UP and right_cursor > 0:
-			right_cursor -= 1
-		elif key == curses.KEY_DOWN and right_cursor < len(Main_menu)-1 :
-			right_cursor += 1
-		elif key == curses.KEY_ENTER or key in [10, 13]:	
-			selected_sub_menu = [right_cursor, selected_main_menu]
+		if key == curses.KEY_UP and selected_left_menu > 0:
+			selected_left_menu -= 1
+		elif key == curses.KEY_DOWN and selected_left_menu < len(Main_menu)-1 :
+			selected_left_menu += 1
+		elif key == curses.KEY_ENTER or key in [10, 13]:
 			break
-			
-		menu_vertical_dic()
 		
-
+		menu_vertical_1(stdscr, Main_menu[selected_main_menu], selected_left_menu, left_screen_hight//2 +1, left_screen_width//2 )
+		for i in range(left_screen_width+1,right_screen_width):
+			stdscr.addstr((right_screen_hight -top_screen_hight)//2 +top_screen_hight,i," ")
+		stdscr.addstr((right_screen_hight -top_screen_hight)//2 +top_screen_hight, (right_screen_width -left_screen_width)//2 +left_screen_width -len(list(Main_menu[selected_main_menu].values())[selected_left_menu])//2, list(Main_menu[selected_main_menu].values())[selected_left_menu])
+		stdscr.refresh()
+		
 	
 
 curses.wrapper(main)
