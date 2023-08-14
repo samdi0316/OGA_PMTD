e2ad797a16853f6a42a7ebc3a4366ec42d6b86cf
samdi0316 <92657345+samdi0316@users.noreply.github.com>
2023-08-14 14:24:21 +0000

    time_setting.py 추가, 시스템적 구현방식 생각중...

--- time_setting.py
+++ time_setting.py
@@ -7,8 +7,27 @@ import getpass
 import sys
 
 ##자료준비
-##자료준비:로고 큰거
-logo_big = [
+##자료준비:전체메뉴
+Main_menu = {
+	'Module' : {'WORKING':':('} , 
+	'Commuication' : {
+		'I2C' : 'Use pin#7 (GPIO0_C2) as SCL and pin#8 (GPIO0_C3) as SDA to coummuicate with I2C device.',
+		'UART' : 'Use UART port at the right side of the device to coummuicate with UART',
+		'WIFI' : 'If you are using OGA_Revision_1.1 config WIFI here'
+		},
+	'GPIO_MENU' : {
+		'INPUT_setting' : 'Show input on GPIO pin',
+		'OUTPUT_setting' : 'Set GPIO to make output signal',
+		'PIN_MAP' : 'Show OGA GPIO pinmap'
+		},
+	'Setting' : {
+		'time_setting' : 'Set time and date',
+		'Device_setting' : 'Set power and screen and more...',
+		'System_setting' : 'Set OGA_PMTE basic setting'}
+}
+
+##자료준비:해골juice
+juice = [
 '⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣀⠤⠴⠶⠲⠦⠤⢀⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀',
 '⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡠⠖⠉⠀⠀⠀⠀⠀⠀⢀⣀⡁⠤⠀⢠⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀',
 '⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⠜⠁⠀⠀⢀⣀⠤⠔⠒⠊⠉⠁⠀⠀⠀⢀⡆⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀',
@@ -37,12 +56,20 @@ logo_big = [
 '⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⢆⣀⠜⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀',
 ]
 
-##자료준비:시작화면 메뉴
-start_screen_menu = ['','Turn off']
+##자료준비:time 로고
+time_logo = [
+'⠀⠀⠀⠀⠀⠀⠀⣰⠛⠛⠛⠛⣆⠀⠀⠀⠀⠀⠀⠀',
+'⠀⠀⣀⣄⣀⣀⡤⠟⠀⠀⠀⠀⠻⢤⣀⣀⣠⣀⠀⠀',
+'⠀⣼⠋⠀⠉⠉⠀⢀⣠⣤⣤⣄⡀⠀⠉⠉⠀⠘⣇⠀',
+'⠸⣅⠀⠀⠀⢀⡾⠋⠀⢠⡄⠀⠙⢷⡀⠀⠀⠀⣸⠇',
+'⠀⠈⣷⠀⠀⣾⠀⠀⠀⢸⡇⠀⠀⠈⣷⠀⠀⣾⠁⠀',
+'⠀⢀⡿⠀⠀⢿⠀⠀⠀⠈⠛⣤⠀⢀⡿⠀⠀⢿⡀⠀',
+'⢰⡋⠀⠀⠀⠈⢷⣄⡀⠀⠀⢀⣠⡾⠁⠀⠀⠀⢹⡆',
+'⠀⢻⡄⠀⣀⣀⠀⠈⠙⠛⠛⠋⠁⠀⣀⣀⠀⢠⡏⠀',
+'⠀⠀⠉⠋⠉⠈⠛⣦⠀⠀⠀⠀⣴⠞⠉⠉⠙⠋⠀⠀',
+'⠀⠀⠀⠀⠀⠀⠀⠹⣤⣤⣤⣤⠏⠀⠀⠀⠀⠀⠀⠀'
 
-##자료준비:yes or no
-y_or_n = ['Yes','No']
-
+]
 ##자료준비:시스템 정보
 USER = getpass.getuser()
 DATE = str(time.strftime('%Y-%m-%d'))
@@ -52,8 +79,8 @@ OS_INFO = RAW_INFO[3].split(" ")
 INFO_FILE = open('info.txt')
 VERS,UPDATE = INFO_FILE.read().split("\n")
 INFO_FILE.close()
-
 start_screen_info = [f'USER : {USER}', DATE, VERS, UPDATE, f"{Architecture_INFO} {OS_INFO[0]}"]
+
 
 ##설정준비
 ##설정준비:색상설정
@@ -75,7 +102,7 @@ curses.curs_set(0)
 ##그리기함수
 ##그리기함수:세로로 메뉴를 나열
 def menu_vertical(selected_window, menu_item, selected_row, middle_hight, middle_width):
-	for index, item in enumerate(menu_item):
+	for index, item in enumerate(menu_item.key()):
 		item_x = middle_width - len(item)//2
 		item_y = middle_hight - len(menu_item)//2 + index
 	
@@ -97,6 +124,25 @@ def menu_vertical_1(selected_window, menu_item, selected_row, middle_hight, midd
 			selected_window.addstr(item_y, item_x, item)
 	selected_window.refresh()
 
+##그리기함수:세로로 메뉴를 나열(딕셔너리 사용)(키값에따라 하단부에 설명 추가)
+def menu_vertical_dic(selected_window, dic_menu ,menu_item, selected_row, middle_hight, middle_width, max_hight, max_width, max_width_left):
+	for index, (key, item) in enumerate(dic_menu.items[menu_item]):
+		key_x = middle_width - len(key)//2
+		key_y = middle_hight - len(dic_item) + index * 2 - 7
+
+		if index == selected_row:
+			selected_window.addstr(key_y, key_x, item, SELECT_COLOR)
+
+			item_x = middle_width - len(item)//2
+			item_y = max_hight - 9 
+			selected_window.addstr(max_width_left + 2, item_y - 2, )
+			for i in range(max_width_left + 2, max_width - 2):
+				selected_window.addstr(item_y -2 , i,"─")
+			selected_window.addstr(item_y, item_x, item)
+		else:
+			selected_window.addstr(key_y, key_x, item)
+
+	selected_window.refresh()
 
 ##그리기함수:가로로 메뉴를 나열
 def menu_horizontal(selected_window, menu_item, selected_column, middle_hight, middle_width):
@@ -132,76 +178,33 @@ def maxyx(selected_window):
 	max_hight, max_width = selected_window.getmaxyx()
 	return max_hight, max_width 
 
+
+
 ##메인
 def main(stdscr):
-	##화면의 최대 사이즈를 먼저 조사
-	stdscr_hight, stdscr_width = stdscr.getmaxyx()
-	half_hight, half_width = stdscr_hight//2 , stdscr_width//2
-
-	##화면에 먼저 로고를 그리고 메뉴를 로고 우측에 생성, 아래에 선택화면 그리기
+	##먼저 화면의 사이즈를 조사하고 분할하기
+	max_hight, max_width = maxyx(stdscr)
+	
+	top_screen_hight , top_screen_width = 0, max_width
+	bot_screen_hight , bot_screen_width = max_hight-2, max_width
+	left_screen_hight, left_screen_width = max_hight-3, 20
+	right_screen_hight, right_screen_width = max_hight-3, max_width 
+	
+	##맨 위 화면은 [사용자	    	현재 매뉴   		날짜] 형식으로 표시
 	stdscr.clear()
-	drew_ASCIIart(stdscr, logo_big, half_hight, half_width - len(logo_big[0])//2-5)
-	menu_vertical_1(stdscr, start_screen_info, 99, half_hight-3, half_width + len(start_screen_info[0])//2+5)
-	menu_horizontal(stdscr, y_or_n, 0, half_hight+7, half_width+ len(start_screen_info[0])//2+5)
-	stdscr.addstr(half_hight +5, half_width + len(start_screen_info[0])//2 +5 -len("Do you want to start OGA_PMTE?")//2, "Do you want to start OGA_PMTE?")
-	stdscr.refresh()
+	stdscr.addstr(top_screen_hight, 0, DATE.rjust(top_screen_width," "), MAIN_COLOR_REVERSE)
+	stdscr.addstr(top_screen_hight, 0, USER,MAIN_COLOR_REVERSE)
+	
+	##맨 밑 화면 첫번째 줄은 D-PAD를 이용하라는 문구, 두번째 줄은 특수 버튼 지정()
+	stdscr.addstr(bot_screen_hight, 0, "Use D-PAD and A button to select".center(bot_screen_width-1, ' '), MAIN_COLOR_REVERSE)
+	stdscr.addstr(bot_screen_hight+1, 0, "SHUT DOWN  ".rjust(bot_screen_width-1, ' '))
 
-	##시작할지 말지를 선택하는 메뉴를 작동
-	selected_column = 0
-	turnoff_menu_select = 0
+	##좌측 상단에 타임 로고 삽입
+	drew_ASCIIart(stdscr, time_logo, 7, 11)
 
-	while True:
-		key = stdscr.getch()
-		if key == curses.KEY_LEFT and selected_column > 0:
-			selected_column = 0
-		elif key == curses.KEY_RIGHT and selected_column < 1:
-			selected_column = 1
-		elif key == curses.KEY_ENTER or key in [10, 13]:
-			 ##Yes를 선택할 경우 MAIN_MENU를 실행
-			if selected_column == 0:
-				os.system("python new_main_menu.py")
-				sys.exit()
-			 ##No를 선택하는 경우 다시한번 물어보기
-			elif selected_column == 1:
-				stdscr.clear()
-				stdscr.addstr(half_hight-1, half_width-len("Do you want to turn off the device?")//2, "Do you want to turn off the device?")
-				menu_horizontal(stdscr, ['Turn off', 'No'], turnoff_menu_select,half_hight+1, half_width)
-				stdscr.refresh()
-				while True:
-					key = stdscr.getch()				
-					if key == curses.KEY_LEFT and turnoff_menu_select > 0:
-						turnoff_menu_select = 0
-					elif key == curses.KEY_RIGHT and turnoff_menu_select < 1:
-						turnoff_menu_select = 1
-					elif key == curses.KEY_ENTER or key in [10, 13]:
-						 ##Turn off를 선택한 경우 시스템을 종료
-						if turnoff_menu_select == 0:
-							stdscr.clear()
-							os.system("sudo shutdown now")
-							sys.exit()
-						 ##No를 선택한 경우에는 다시 작동할지말지 선택하는 메뉴로 복귀
-						elif turnoff_menu_select == 1:
-							turnoff_menu_select = 0
-							break
-					stdscr.clear()
-					stdscr.addstr(half_hight-1, half_width-len("Do you want to turn off the device?")//2, "Do you want to turn off the device?")
-					menu_horizontal(stdscr, ['Turn off', 'No'], turnoff_menu_select,half_hight+1, half_width)
-					stdscr.refresh()
-			if turnoff_menu_select == 1:
-				stdscr.clear()
-				break  
-		
-		stdscr.clear()
-		drew_ASCIIart(stdscr, logo_big, half_hight, half_width - len(logo_big[0])//2-5)
-		menu_vertical_1(stdscr, start_screen_info, 99, half_hight-3, half_width + len(start_screen_info[0])//2+5)
-		menu_horizontal(stdscr, y_or_n, selected_column, half_hight+7, half_width+ len(start_screen_info[0])//2+5)
-		stdscr.addstr(half_hight +5, half_width + len(start_screen_info[0])//2 +5 -len("Do you want to start OGA_PMTE?")//2, "Do you want to start OGA_PMTE?")
-		stdscr.refresh()
+	stdscr.getch()
 
-	stdscr.clear()
-	stdscr.refresh()
-	time.sleep(3)
-		
-			
 
 curses.wrapper(main)
+##│┐┌ ─ ┘└
+	
